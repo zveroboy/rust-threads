ffc73e04dce1e7762ec2645d456b6b82277fc0fa
Nikolay Kolesnikov <zveroboy85@gmail.com>
2022-01-14 18:43:39 +0300

    Simplified

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::thread;
 
 const THRESHOLD: usize = 3;
 
-fn sqare (x: i32) -> i32 { 
+fn double (x: i32) -> i32 { 
     x * 2 
 }
 
@@ -18,38 +18,38 @@ where
 
     if input.len() <= THRESHOLD {
         println!("processed without thread {:?}", input);
-        return input.into_iter().map(f).collect();
+        return input
+                .into_iter()
+                .map(f)
+                .collect();
     }
 
-    let chanks = input.chunks(THRESHOLD);
-    let mut children = vec![];
-   
-    for chank in chanks {
-        let owned_chank = chank.to_vec();
-        children.push(thread::spawn(move || -> Vec<R> {
-            println!("hello from thread {:?}", owned_chank);
-            owned_chank.iter()
-                .map(|&item| f(item) )
-                .collect()
-        }))
-    }
-
-    children
+    input
+        .chunks(THRESHOLD)
         .into_iter()
+        .map(|chank| -> thread::JoinHandle<Vec<R>> {
+            let owned_chank = chank.to_vec();
+            thread::spawn(move || -> Vec<R> {
+                println!("hello from thread {:?}", owned_chank);
+                owned_chank
+                    .iter()
+                    .map(|&item| f(item) )
+                    .collect()
+            })
+        })
         .flat_map(|thread_result| thread_result.join().unwrap())
         .collect()
 }
 
-
 fn main() {
     println!("======== Results w/o threads ========");
 
-    let res_i32_1 = helper(vec![10, 20, 30], sqare);
+    let res_i32_1 = helper(vec![10, 20, 30], double);
     println!("{:?}", res_i32_1);
 
     println!("======== Results with threads ========");
 
-    let res_i32_2 = helper(vec![10, 20, 30, 40, 50, 60, 70], sqare);
+    let res_i32_2 = helper(vec![10, 20, 30, 40, 50, 60, 70], double);
     println!("{:?}", res_i32_2);
 
     println!("======== Results w/o threads ========");
@@ -62,3 +62,4 @@ fn main() {
     let res_str_2 = helper(vec!["aa", "bb", "cc", "dd", "ee", "ff"], uppercase);
     println!("{:?}", res_str_2);
 }
+
